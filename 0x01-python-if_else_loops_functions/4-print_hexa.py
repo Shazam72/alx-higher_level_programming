d0b5ecfd5b275adebc769427fbc85156eee25c6e
leviathan72 <jfrancoiszouba72@gmail.com>
2023-06-06 19:26:46 +0200

    Change for format function

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for n in list(range(0, 99)):
-    print(f"0x{n:x}")
+    print("0x{:x}".format(n))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in list(range(0, 100)):
-    print(f"{n:02d}, " if n != 99 else f"{n:02d}", end='')
+    print("{:02d}, ".format(n) if n != 99 else "{:02d}".format(n), end='')
 print()
