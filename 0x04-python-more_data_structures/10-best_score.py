05136c81ba5ab59adca4a34b272c51feb778ce90
leviathan72 <jfrancoiszouba72@gmail.com>
2023-06-14 20:40:58 +0200

    Update task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return 'None'
+        return None
     sorted_scores = sorted(a_dictionary.items(),
                            key=lambda v: v[1],
                            reverse=True)
