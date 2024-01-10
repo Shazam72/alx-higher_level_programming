3549c1262446c2921c091a2cecbef35be4faf9f5
Shazam72 <jfrancoiszouba72@gmail.com>
2024-01-10 18:00:34 +0100

    Deleting old files

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-exports.addMeMaybe = (number, theFunction) => {
-  theFunction(number + 1);
-};
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : process.argv[2]);
