b0f3059d4ee7c919efa6dce13d0a169e7dd3cd6d
Shazam72 <jfrancoiszouba72@gmail.com>
2024-01-11 14:54:52 +0100

    Task 1: updating 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that sequest to an url and display the value of X-Request-Id."""
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as resp:
+    print(resp.headers['X-Request-Id'])
