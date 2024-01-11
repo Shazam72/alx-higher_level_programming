f43ee24dc4ad28dfd59702c9bd6cebbd0516665a
Shazam72 <jfrancoiszouba72@gmail.com>
2024-01-12 00:37:25 +0100

    Update 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes your github credentials and displays your id using gith api."""
+import requests
+import sys
+
+headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f"Bearer {sys.argv[2]}",
+    'X-GitHub-Api-Version': '2022-11-28'
+}
+url = f"https://api.github.com/users/{sys.argv[1]}"
+with requests.get(url, headers) as resp:
+    try:
+        if resp.status_code != 200:
+            print(None)
+        else:
+            data = resp.json()
+            print(data['id'])
+    except KeyError:
+        print(None)
