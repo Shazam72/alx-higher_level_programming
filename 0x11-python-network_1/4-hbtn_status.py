967487ddb99eb46188f62d2ad72b7ed411192f39
Shazam72 <jfrancoiszouba72@gmail.com>
2024-01-11 22:53:51 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """Fetches at https://alx-intranet.hbtn.io/status."""
 import requests
 
-resp = requests.get('https://alx-intranet.hbtn.io/status')
-data = resp.content.decode()
-print('Body response')
-print(f"\t- type: {data.__class__}")
-print(f"\t- content: {data}")
+if __name__ == '__main__':
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    data = resp.content.decode()
+    print('Body response')
+    print(f"\t- type: {data.__class__}")
+    print(f"\t- content: {data}")
