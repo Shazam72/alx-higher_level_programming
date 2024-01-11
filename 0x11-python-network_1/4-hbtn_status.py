114d6ebc86519b4eecff995bd6534deb24e7ff97
Shazam72 <jfrancoiszouba72@gmail.com>
2024-01-11 23:00:02 +0100

    with statement in  4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,8 @@
 import requests
 
 if __name__ == '__main__':
-    resp = requests.get('https://alx-intranet.hbtn.io/status')
-    data = resp.content.decode()
-    print('Body response')
-    print(f"\t- type: {data.__class__}")
-    print(f"\t- content: {data}")
+    with requests.get('https://alx-intranet.hbtn.io/status') as resp:
+        data = resp.content.decode()
+        print('Body response')
+        print(f"\t- type: {data.__class__}")
+        print(f"\t- content: {data}")
