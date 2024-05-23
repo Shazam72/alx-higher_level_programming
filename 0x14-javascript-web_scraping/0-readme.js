30ef33122b40bdb90a31ff373820d1cf740539a7
Shazam72 <jfrancoiszouba72@gmail.com>
2024-05-23 04:21:24 +0200

    Update task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-	if (error){
-		console.log(error);
-		return
-	}
-	console.log(content);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(content);
 });
